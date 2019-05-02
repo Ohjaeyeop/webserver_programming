7aad406c3efead860e88ac07c4240dc32fce6d0c
20171158 <ohbcj0831@unist.ac.kr>
2019-05-02 17:57:38 +0900

    assign

--- server_pool.c
+++ server_pool.c
@@ -25,6 +25,15 @@ void sendall(int sock, char* msg) {
   }
 }
 
+void sendall_i(int sock, char* msg) {
+  int length = 200;
+  int bytes;
+  while(length > 0) {
+    bytes = send(sock, msg, length, 0);
+    length = length - bytes;
+  }
+}
+
 int main( int argc, char *argv[] ) {
   int newsockfd[50];
   int sockfd, portno = PORT;
@@ -71,6 +80,7 @@ int main( int argc, char *argv[] ) {
 
   printf("Server is running on port %d\n", portno);
   int client_count = 0;
+  threadpool thpool = thpool_init(4);
   while (1) {
     newsockfd[client_count] = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
     if ( newsockfd[client_count] == -1 ){
@@ -79,13 +89,97 @@ int main( int argc, char *argv[] ) {
     }
 
     // TODO : multi threading part
+    thpool_add_work(thpool, (void*)respond, newsockfd[client_count]);
 
     client_count++;
   }
+  thpool_destroy(thpool);
 
   return 0;
 }
 
 void respond(int sock) {
+  char buffer[9000];
+  char file_path[50]="\0";
+  int i, j, flag, size;
+  char message[] = "HTTP/1.1 200 OK\r\nContent-type: ";
 
+  bzero(buffer, 9000);
+  recv(sock, buffer, 9000, 0);
+
+  i = 0; j = 0; flag = 0;
+  while(1)
+  {
+    if(flag == 1)
+    {
+      file_path[j] = buffer[i];
+      j++;
+    }
+    if(buffer[i] == '/')
+      flag = 1;
+    i++;
+    if(flag == 1 && buffer[i] == ' ')
+    {
+      break;
+    }
+  }
+  if(strlen(file_path) == 0)
+    strcpy(file_path, "index.html");
+
+  char index[250], tmp[50], img[300];
+
+  strcpy(tmp, file_path);
+
+  flag = 1;
+  if(strstr(file_path, "html") != NULL)
+    strcat(message, "text/html;\r\n\r\n");
+  else if(strstr(file_path, "css") != NULL)
+    strcat(message, "text/css;\r\n\r\n");
+  else if(strstr(file_path, "js") != NULL)
+    strcat(message, "text/js;\r\n\r\n");
+  else if(strstr(file_path, "jpg") != NULL)
+  {
+    strcat(message, "image/jpg;\r\n\r\n");
+    flag = 2;
+  }
+  else
+    flag = 0;
+
+  if(flag == 1)
+  {
+    FILE *fp;
+    fp = fopen(tmp, "r");
+
+    sendall(sock, message);
+    while(1)
+    {
+      size = fread(index, sizeof(char), 200, fp);
+      sendall(sock, index);
+      bzero(index, 250);
+      if(feof(fp))
+        break;
+    }
+    sendall(sock, "\r\n\r\n");
+    fclose(fp);
+  }
+  else if(flag == 2)
+  {
+    FILE *fp;
+    fp = fopen(tmp, "rb+");
+
+    sendall(sock, message);
+    while(1)
+    {
+      size = fread(img, sizeof(char), 200, fp);
+      sendall_i(sock, img);
+      bzero(img, 200);
+      if(feof(fp))
+        break;
+    }
+    sendall(sock, "\r\n\r\n");
+    fclose(fp);
+  }
+  else
+    sendall(sock, "HTTP/1.1 404 NOT FOUND\r\n" );
+  close(sock);
 }
--- server_single.c
+++ server_single.c
@@ -24,6 +24,15 @@ void sendall(int sock, char* msg) {
   }
 }
 
+void sendall_i(int sock, char* msg) {
+  int length = 200;
+  int bytes;
+  while(length > 0) {
+    bytes = send(sock, msg, length, 0);
+    length = length - bytes;
+  }
+}
+
 int main( int argc, char *argv[] ) {
   int newsockfd[50];
   int sockfd, portno = PORT;
@@ -86,35 +95,87 @@ int main( int argc, char *argv[] ) {
 }
 
 void respond(int sock) {
-  int offset, bytes;
   char buffer[9000];
-  bzero(buffer,9000);
+  char file_path[50]="\0";
+  int i, j, flag, size;
+  char message[] = "HTTP/1.1 200 OK\r\nContent-type: ";
 
-  offset = 0;
-  bytes = 0;
-  do {
-    bytes = recv(sock, buffer + offset, 1500, 0);
-    offset += bytes;
-    if (strncmp(buffer + offset - 4, "\r\n\r\n", 4) == 0) break;
-  } while (bytes > 0);
+  bzero(buffer, 9000);
+  recv(sock, buffer, 9000, 0);
 
-  if (bytes < 0) {
-    printf("recv() error\n");
-    return;
+  i = 0; j = 0; flag = 0;
+  while(1)
+  {
+    if(flag == 1)
+    {
+      file_path[j] = buffer[i];
+      j++;
+    }
+    if(buffer[i] == '/')
+      flag = 1;
+    i++;
+    if(flag == 1 && buffer[i] == ' ')
+    {
+      break;
+    }
   }
-  else if (bytes == 0) {
-    printf("Client disconnected unexpectedly\n");
-    return;
+  if(strlen(file_path) == 0)
+    strcpy(file_path, "index.html");
+
+  char index[250], tmp[50], img[300];
+
+  strcpy(tmp, file_path);
+
+  flag = 1;
+  if(strstr(file_path, "html") != NULL)
+    strcat(message, "text/html;\r\n\r\n");
+  else if(strstr(file_path, "css") != NULL)
+    strcat(message, "text/css;\r\n\r\n");
+  else if(strstr(file_path, "js") != NULL)
+    strcat(message, "text/js;\r\n\r\n");
+  else if(strstr(file_path, "jpg") != NULL)
+  {
+    strcat(message, "image/jpg;\r\n\r\n");
+    flag = 2;
   }
+  else
+    flag = 0;
 
-  buffer[offset] = 0;
-  printf("%s\n", buffer);
+  if(flag == 1)
+  {
+    FILE *fp;
+    fp = fopen(tmp, "r");
 
-  char message[] = "HTTP/1.1 200 OK\r\nContent-Type: text/html;\r\n\r\n<html><body>Hello World!</body></html>\r\n\r\n";
+    sendall(sock, message);
+    while(1)
+    {
+      size = fread(index, sizeof(char), 200, fp);
+      sendall(sock, index);
+      bzero(index, 250);
+      if(feof(fp))
+	break;
+    }
+    sendall(sock, "\r\n\r\n");
+    fclose(fp);
+  }
+  else if(flag == 2)
+  {
+    FILE *fp;
+    fp = fopen(tmp, "rb+");
 
-  sendall(sock, message);
-
-  printf("close\n");
-  shutdown(sock, SHUT_RDWR);
+    sendall(sock, message);
+    while(1)
+    {
+      size = fread(img, sizeof(char), 200, fp);
+      sendall_i(sock, img);
+      bzero(img, 200);
+      if(feof(fp))
+	break;
+    }
+    sendall(sock, "\r\n\r\n");
+    fclose(fp);
+  }
+  else
+    sendall(sock, "HTTP/1.1 404 NOT FOUND\r\n" );
   close(sock);
 }
